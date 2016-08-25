c20dce507478a1cb9aa261ba5a24a5d1a370718c
AndyKazma <slavek@123321.pl>
2016-08-25 15:58:10 +0200

    First commit, adjustments added

---
+++ App.js
@@ -0,0 +1,31 @@
+// komunikacja z API
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': <[42]>,
+  'X-Auth-Token': <[6fc965c0225207afef65222d2c3f7626]>
+};
+$.ajaxSetup({
+	headers: myHeaders
+});
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+}
